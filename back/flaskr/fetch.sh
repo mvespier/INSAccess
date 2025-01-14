d8f403a54b02954b0e80adb8ad47f7e6e5d961a0
Raphael Senellart <raphael.senellart@insa-rouen.fr>
2025-01-14 18:34:41 +0100

    finished fecth.sh

--- back/flaskr/fetch.sh
+++ back/flaskr/fetch.sh
@@ -7,4 +7,5 @@ fichier=$3
 curl "http://agendas.insa-rouen.fr/2024/day.php?cpath=&getdate=$date&cal%5B%5D=$depart" |
 perl -pe 's/.*new EventData\(.*?\{(.*)\}.*/=TOTO=>$1/' |
 grep "=TOTO=>" |
-perl -pe 's/.*"display_start\\";s:\d+:\\"(.*?)\\".*"display_end\\";s:\d+:\\\"(.*?)\\".*"event_text\\";s:\d+:\\\"(.*?)\\".*"description\\";s:\d+:\\\".*?%2F%3E.*?%2F%3E.*?%2F%3E(.*?)%3C.*?\\".*"location\\";s:\d+:\\\"(.*?)\\".*/$1\t$2\t$3\t$4\t$5\t/' > $fichier
+perl -pe 's/.*"display_start\\";s:\d+:\\"(.*?)\\".*"display_end\\";s:\d+:\\\"(.*?)\\".*"event_text\\";s:\d+:\\\"(.*?)\\".*"description\\";s:\d+:\\\".*?%2F%3E.*?%2F%3E.*?%2F%3E(.*?)%3C.*?\\".*"location\\";s:\d+:\\\"(.*?)\\".*/$1\t$2\t$3\t$4\t$5\t/' | 
+sed "s/^/$date\t$depart\t/" > $fichier
