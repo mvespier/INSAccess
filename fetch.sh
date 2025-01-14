b41a1ebb4ae23acad7612011d42dbda7c21774ff
Raphael Senellart <raphael.senellart@insa-rouen.fr>
2025-01-14 16:19:44 +0100

    restructured everything

--- fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-date=$1
-depart=$2
-fichier=$3
-
-curl "http://agendas.insa-rouen.fr/2024/day.php?cpath=&getdate=$date&cal%5B%5D=$depart" | perl -pe 's/.*new EventData\(.*?\{(.*)\}.*/=TOTO=>$1/' | grep "=TOTO=>" | perl -pe 's/.*"display_start\\";s:\d+:\\"(.*?)\\".*"display_end\\";s:\d+:\\\"(.*?)\\".*"event_text\\";s:\d+:\\\"(.*?)\\".*"description\\";s:\d+:\\\".*?%2F%3E.*?%2F%3E.*?%2F%3E(.*?)%3C.*?\\".*"location\\";s:\d+:\\\"(.*?)\\".*/20250227\t2024-STPI1\t$1\t$2\t$3\t$4\t$5\t/' > $fichier
--- test.py
+++
@@ -1,8 +0,0 @@
-from markupsafe import escape
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
