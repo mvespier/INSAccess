1ab11f201a44564a047c1427efa424ba80969f16
Onniryss <145003114+Onniryss@users.noreply.github.com>
2025-01-15 22:04:20 +0100

    ajusted the timeline and fixed the position of the events

--- front/index.js
+++ front/index.js
@@ -3,7 +3,7 @@ const hours_end = ["9h30", "11h15", "13h", "14h45", "16h30", "18h15"]
 
 function SingleEvent({start, end, label}){
   return (
-    <div className={`event start-${start+1} end-${end+1} securities`}>
+    <div className={`event start-${start} end-${end} securities`}>
       <p className="title">{label}</p>
       <p className="time">{hours_start[start]} - {hours_end[end-1]}</p>
     </div>
@@ -14,12 +14,30 @@ function AllEvents(){
     return (
       <>
         <SingleEvent start={0} end={1} label={"PROGRAV"} />
-        <SingleEvent start={2} end={3} label={"AUTO"} />
+        <SingleEvent start={1} end={3} label={"AUTO"} />
       </>
     );
 }
 
+function TimeBar(){
+  return (
+    <>
+        <div class="space"></div>
+        <div class="time-marker">8h00</div>
+        <div class="time-marker">9h30</div>
+        <div class="time-marker">11h15</div>
+        <div class="time-marker">13h00</div>
+        <div class="time-marker">14h45</div>
+        <div class="time-marker">16h30</div>
+        <div class="time-marker">18h15</div>
+    </>
+  );
+}
 
-const node = document.getElementById('events-mon');
-const root = ReactDOM.createRoot(node);
-root.render(<AllEvents />);
+const events1 = document.getElementById('events-mon');
+const rootEvents1 = ReactDOM.createRoot(events1);
+rootEvents1.render(<AllEvents />);
+
+const time_bar = document.getElementById('hours');
+const rootTimeBar = ReactDOM.createRoot(time_bar);
+rootTimeBar.render(<TimeBar />);
