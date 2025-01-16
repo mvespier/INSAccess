3f369d7cdb500ffc8872fe2ae83508d0e7ee0aca
Onniryss <145003114+Onniryss@users.noreply.github.com>
2025-01-16 15:25:49 +0100

    added subdivisions to the hour columns and made it so there is a slight padding between the events. We can now add events by hour and not by index

--- front/index.js
+++ front/index.js
@@ -1,11 +1,20 @@
-const hours_start = ["8h00", "9h45", "11h30", "13h15", "15h00", "16h45"]
-const hours_end = ["9h30", "11h15", "13h", "14h45", "16h30", "18h15"]
+const hours_start = ["8h00", "8h30", "9h00", "9h45", "10h15", "10h45", "11h30", "12h00", "12h30", "13h15", "13h45", "14h15", "15h00", "15h30", "16h00", "16h45", "17h15", "17h45"]
+const hours_end = ["8h15", "8h45", "9h30", "10h00", "10h30", "11h15", "11h45", "12h15", "13h00", "13h30", "14h00", "14h45", "15h15", "15h45", "16h30", "17h00", "17h30", "18h15"]
+const hours_timeline = ["8h00", "9h45", "11h30", "13h15", "15h00", "16h45", "18h15"]
 
-function SingleEvent({start, end, label}){
+function SingleEvent({start_time, end_time, label, color}){
+  let start_index = hours_start.indexOf(start_time)+1;
+  let end_index = hours_end.indexOf(end_time)+2;
+
+  const eventStyle = {
+    gridRowStart: start_index, 
+    gridRowEnd: end_index
+  };
+
   return (
-    <div className={`event start-${start} end-${end} securities`}>
+    <div className={`event ${color}`} style={eventStyle}>
       <p className="title">{label}</p>
-      <p className="time">{hours_start[start]} - {hours_end[end-1]}</p>
+      <p className="time">{hours_start[start_index]} - {hours_end[end_index]}</p>
     </div>
   );
 }
@@ -13,8 +22,8 @@ function SingleEvent({start, end, label}){
 function AllEvents(){
     return (
       <>
-        <SingleEvent start={0} end={1} label={"PROGRAV"} />
-        <SingleEvent start={1} end={3} label={"AUTO"} />
+        <SingleEvent start_time={"8h00"} end_time={"9h30"} label={"PROGRAV"} color={"colRed"} />
+        <SingleEvent start_time={"9h45"} end_time={"13h00"} label={"AUTO"} color={"colBlue"}/>
       </>
     );
 }
@@ -23,7 +32,7 @@ function TimeBar(){
   const hours = [];
   hours.push(<div key={-1} className="spacer"></div>);
   for (let i = 0; i < hours_start.length; i++){
-    hours.push(<div key={i} className="time-marker">{hours_start[i]}</div>)
+    hours.push(<div key={i} className="time-marker">{hours_timeline[i]}</div>)
   }
   return (
     <>
