9244fc8a59dd3c897f3ae76db1cc6597f14f5e62
Cookie <raphael@senellart.com>
2025-01-17 18:18:29 +0100

    finished v2 of fetch.py (way more robust)

--- back/app/utils/fetch.py
+++ back/app/utils/fetch.py
@@ -13,14 +13,17 @@ def description_parsing(description, list_of_depart, depart_year):
 
     name = final_description.pop(i) if i!=None else 'none'
 
-    
-    
-    
 
-    """if i==None:
-        return [],"none",final_description
-    return final_description [:i], final_description[i], final_description[i+1:]"""
-    return [],name,[]
+    department_set = set(i+depart_year for i in list_of_depart)
+
+    depart_in_desc, td_group_in_desc = [], []
+    for item in final_description:
+        if item in department_set:
+            depart_in_desc.append(item)
+        else:
+            td_group_in_desc.append(item)
+
+    return [item for item in td_group_in_desc if item != 'examens'], name, depart_in_desc
 
 def get_name_index(list : list):
     for i in range(len(list)):
@@ -33,7 +36,7 @@ def item_to_string(item):
     return item.text if item != None else "None"
 
 
-def xml_to_list(url : str) -> list:
+def xml_to_list(url : str, list_of_depart, depart_year) -> list:
         
     response = requests.get(url)
 
@@ -55,7 +58,7 @@ def xml_to_list(url : str) -> list:
             location = item_to_string(item.find("ev:location", namespaces))
 
             
-            group_td, teacher, group_depart =  description_parsing(description)
+            group_td, teacher, group_depart =  description_parsing(description, list_of_depart, depart_year)
 
             output.append((date, start_hour, end_hour, location, teacher, title, group_td, group_depart))
         
@@ -64,18 +67,18 @@ def xml_to_list(url : str) -> list:
         print(f"Failed to fetch XML data. HTTP Status Code: {response.status_code}")
         return []
 
-def get_calendar_data(current_year :int, department : str , year_depart : int, date : int, period :str) -> list :
+def get_calendar_data(current_year :int, department : str , depart_year : int, date : int, period :str) -> list :
 
     list_of_depart = ["CGC", "EP", "GCU", "GM", "GPGR", "ITI", "MECA", "PERF-E", "PERF-II", "PERF-ISP", "PERF-NI"]
     list_of_period = ["day", "week", "month"]
 
-    if ( (department in list_of_depart) and (3 <= int(year_depart) <= 5 ) and (period in list_of_period) ) or (department =="STPI" and 1<= int(year_depart) <= 2) :
+    if ( (department in list_of_depart) and (3 <= int(depart_year) <= 5 ) and (period in list_of_period) ) or (department =="STPI" and 1<= int(depart_year) <= 2) :
 
-        url = "http://agendas.insa-rouen.fr/rss/rss2.0.php?cal=" + current_year+"-"+ department + year_depart + "&cpath=&rssview=" + period + "&getdate=" + date
+        url = "http://agendas.insa-rouen.fr/rss/rss2.0.php?cal=" + current_year+"-"+ department + depart_year + "&cpath=&rssview=" + period + "&getdate=" + date
         print("-"*150)
         print("URL : " + url)
         print("-"*150)
-        out = xml_to_list(url)
+        out = xml_to_list(url, list_of_depart, depart_year)
 
         if (len(out) == 0) :
             print("ERROR : no match found")
