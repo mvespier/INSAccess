b9f2c4d3a26a9f622f9b6b7f407379faccc196b4
jgalhardo <jules.galhardo@insa-rouen.fr>
2025-01-28 09:41:57 +0100

    doesn't work anymore bc i tried to make the dateUtils functions into a class, wip

---
+++ front/insaccess/src/js/constantes.js
@@ -0,0 +1,6 @@
+const hours_timeline = ["8h00", "9h45", "11h30", "13h15", "15h00", "16h45", "18h30", "20h15"];
+const dayList = [ "Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+const minWidth = 850;
+
+
+export default { hours_timeline, dayList, minWidth };
