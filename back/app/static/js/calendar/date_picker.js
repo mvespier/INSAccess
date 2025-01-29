83546e49b1878bcc654fd13860f4d1b3f54472ce
Cookie <raphael@senellart.com>
2025-01-29 18:55:31 +0100

    improved calendar element

--- back/app/static/js/calendar/date_picker.js
+++ back/app/static/js/calendar/date_picker.js
@@ -1,17 +1,8 @@
-function openDatePicker() {
-    document.getElementById('dateInput')._flatpickr.open();
-}
-
-function updateDate(selectedDates, dateStr) {
-    document.getElementById('dateDisplay').textContent = dateStr;
-}
-
 window.onload = function() {
-    const today = new Date().toISOString().split('T')[0];
-    document.getElementById('dateDisplay').textContent = today;
     flatpickr("#dateInput", {
-        defaultDate: today,
-        onChange: updateDate,
-        dateFormat: "F j, Y",
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+        defaultDate: new Date()
     });
 };
