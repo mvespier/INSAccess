3c91b24dcdde9593c107fe6d20b0713ee97c8634
jgalhardo <jules.galhardo@insa-rouen.fr>
2025-02-03 13:25:57 +0100

    cleanup of the code, with only one function for the calendar and the associations

--- front_experiment/insaccess/src/pages/tests.js
+++ front_experiment/insaccess/src/pages/tests.js
@@ -1,7 +1,15 @@
 import { useMediaQuery } from 'react-responsive'
+import data from '../data.json'
+import { DataContainer } from '../js/randomUtils';
 
 function Home(){
     const isMobile = useMediaQuery({ maxWidth: 850 });
+    console.log(data)
+    let data_obj = new DataContainer(data)
+    console.log(data_obj)
+    console.log(data_obj.data)
+    console.log(typeof(data))
+    console.log(typeof(data_obj))
 
     return (
         <div>
