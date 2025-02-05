1ff4ca80fd0adf3cd2aa1e13c605d3b81048c722
ImJustCookie <raphael@senellart.com>
2025-02-05 10:55:09 +0100

    tried to work on merging

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -10,27 +10,32 @@ from .. import db, mail, serializer
 from ..utils.fetch import get_calendar_data
 from ..utils.token import confirm_token, generate_token
 
-main = Blueprint('main', __name__)
-
+main = Blueprint('main', __name__, static_folder='static/react/static', template_folder='static/react')
 
 @main.route('/')
-@login_required
-def default_page():
-    return render_template('index.html')
-
-@main.route('/redirect')
-def redirect_test():
-    return redirect(url_for('main.default_page'))
-
-@main.route('/<test>/redirect')
-def var_test(test):
-    return 'TOTO'
+@main.route('/<path:path>')
+def serve_react(path="index.html"):
+    return send_from_directory('static/react', path)
 
 
-@main.route('/calendar')
-def calendar():
-    return render_template('calendar_example.html')
+# @main.route('/')
+# @login_required
+# def default_page():
+#     return render_template('index.html')
 
-@main.route('/robots.txt')
-def robots():
-    return send_from_directory('static', 'robots.txt')
+# @main.route('/redirect')
+# def redirect_test():
+#     return redirect(url_for('main.default_page'))
+
+# @main.route('/<test>/redirect')
+# def var_test(test):
+#     return 'TOTO'
+
+
+# @main.route('/calendar')
+# def calendar():
+#     return render_template('calendar_example.html')
+
+# @main.route('/robots.txt')
+# def robots():
+#     return send_from_directory('static', 'robots.txt')
